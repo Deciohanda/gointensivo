a588f79b50e29a6eeee8811c831ebb4a442396a9
Decio Handa <decio.handa@smarketsolutions.com.br>
2023-03-07 21:44:11 -0300

    first commit

---
+++ internal/infra/database/order_repository.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/Deciohanda/gointensivo/internal/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{
+		Db: db,
+	}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	_, err := r.Db.Exec("Insert into orders (id, price, tax, final_price) Values(?,?,?,?)",
+		order.Id, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *OrderRepository) GetTotal() (float64, error) {
+	var total float64
+	err := r.Db.QueryRow("select count (1) from orders").Scan(&total)
+	if err != nil {
+		return 0, err
+	}
+	return total, nil
+}
